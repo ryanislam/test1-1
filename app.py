63d39d0c16b2237f039628ece24b4c1ece46122c
Sami Alfattani <sami_alfattani@hotmail.com>
2019-11-16 01:57:05 +0300

    add Flask Features

---
+++ app.py
@@ -0,0 +1,45 @@
+
+
+from flask import Flask, url_for, render_template, request
+
+
+app = Flask(__name__)
+app.secret_key = 'fbd1eefad885bf835e1d5ea884244221'
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def home():
+  return render_template('index.html', title='Home')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html', title='About')
+
+@app.route('/table_example')
+def table_example():
+  return render_template('table_example.html', title='Data Table')
+
+
+@app.route('/login')
+def login():
+  return render_template('login.html', title='Login')
+
+
+@app.route('/handle_data', methods=['GET','POST'])
+def handle_data():
+  us = request.form['username']
+  pw = request.form['password']
+
+  return render_template('index.html', 
+    title='Home', 
+    msg=f"{us} is registered {pw}",
+    msg_class= "alert alert-success")
+
+
+
+app.run(debug=True, port='5555')
+
+
